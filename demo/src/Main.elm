8fb856baa02c57f7734cd62461b072f7d6b51515
Aksel Wester <aksel@wester.co>
2018-05-01 12:48:58 +0200

    Legger til live-koding prosjekt

---
+++ demo/src/Main.elm
@@ -0,0 +1,73 @@
+module Main exposing (..)
+
+import Html exposing (Html, text, div, h1, h2, img, ul, li)
+import Html.Attributes exposing (src)
+
+
+---- MODEL ----
+
+
+type alias Model =
+    { forelesere : List String }
+
+
+init : Model
+init =
+    { forelesere =
+        [ "Johanne", "Aksel" ]
+    }
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> Model
+update msg model =
+    model
+
+
+
+---- VIEW ----
+
+
+listeElement : String -> Html Msg
+listeElement streng =
+    li [] [ text streng ]
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ img [ src "/logo.svg" ] []
+        , h1 [] [ text "Forelesning INF2810" ]
+        , h2 [] [ text "Forelesere" ]
+        , ul []
+            (List.map
+                listeElement
+                model.forelesere
+            )
+        ]
+
+
+
+---- PROGRAM ----
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { view = view
+        , init = ( init, Cmd.none )
+        , update = updateWithCommand
+        , subscriptions = always Sub.none
+        }
+
+
+updateWithCommand : Msg -> Model -> ( Model, Cmd Msg )
+updateWithCommand msg model =
+    ( update msg model, Cmd.none )
